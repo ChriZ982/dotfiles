5fbb46967b06f819b3e123ecd6de63839e639a72
Christopher Zinda <christopher.zinda@posteo.de>
2023-05-06 14:10:02 +0000

    Add starship and multiple improvements

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles repo
+
+Original repo at [Gitlab/ChriZ98](https://gitlab.com/ChriZ98/dotfiles)
+
+This repo contains my dotfiles configuration
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,11 @@
 sudo apt update && sudo apt install -y \
     fish \
     bat
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
+
+chsh -s "$(which fish)"
+
+mkdir -p ~/.config/fish
+cp ./.config/fish/config.fish ~/.config/fish/config.fish
+cp ./.config/starship.toml ~/.config/starship.toml
+cp ./.vimrc ~/.vimrc
