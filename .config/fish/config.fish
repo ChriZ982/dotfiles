2943cf452f124f29147196203eb982fc3a5e716b
Christopher Zinda <christopher.zinda@posteo.de>
2023-06-05 01:07:15 +0200

    Add fish config including aliases and improve starship conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,31 @@
+#!/usr/local/bin/fish
+
+set -U fish_greeting ""
+
+abbr -a ls exa
+abbr -a l exa -l
+abbr -a ll exa -al
+
+if type -q bat
+    abbr -a cat bat -p --paging=never
+else if type -q batcat
+    abbr -a cat batcat -p --paging=never
+end
+
+abbr -a gst git status
+abbr -a gco git checkout
+abbr -a ga git add
+abbr -a gd git diff --ignore-all-space
+abbr -a gdca git diff --cached --ignore-all-space
+abbr -a gcm git commit -m
+abbr -a gp git push
+abbr -a gpl git pull
+
+abbr -a di docker images
+abbr -a dps docker ps -a
+
 starship init fish | source
+
+if type -q kubectl
+    kubectl completion fish | source
+end
