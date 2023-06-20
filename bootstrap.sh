c16a709299229e383201647abf1633ab1af7be9a
Christopher Zinda <christopher.zinda@posteo.de>
2023-06-20 21:52:53 +0000

    Add exa install and fix shellcheck suggestion

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,9 @@ OS=$(grep -ioP '^ID_LIKE=\K.+' /etc/os-release)
 
 if [[ $OS == "debian" ]]; then
     sudo apt update && sudo apt install -y \
-        fish \
-        bat
+        bat \
+        exa \
+        fish
     curl -sS https://starship.rs/install.sh | sh -s -- --yes
 # elif [[ $OS == "arch" ]]; then
 
@@ -15,7 +16,7 @@ fi
 chsh -s "$(which fish)"
 
 mkdir -p ~/.config/fish/completions
-ln -f -s $(pwd)/.config/fish/config.fish ~/.config/fish/config.fish
-ln -f -s $(pwd)/.config/fish/completions/docker.fish ~/.config/fish/completions/docker.fish
-ln -f -s $(pwd)/.config/starship.toml ~/.config/starship.toml
-ln -f -s $(pwd)/.vimrc ~/.vimrc
+ln -f -s "$(pwd)/.config/fish/config.fish" ~/.config/fish/config.fish
+ln -f -s "$(pwd)/.config/fish/completions/docker.fish" ~/.config/fish/completions/docker.fish
+ln -f -s "$(pwd)/.config/starship.toml" ~/.config/starship.toml
+ln -f -s "$(pwd)/.vimrc" ~/.vimrc
